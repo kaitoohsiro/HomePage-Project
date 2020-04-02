9e542d5861ba5ebba0e05fda411fc65b47fe7cdc
kmatsu <kmatsu.pro@gmail.com>
2020-04-02 23:17:42 +0900

    インデント整理

--- resources/js/pages/productComponents/Project.vue
+++ resources/js/pages/productComponents/Project.vue
@@ -1,15 +1,17 @@
 <template>
 <div>
 	<div v-for="project in ProductProjects" v-bind:key="project.id" class="project">
-      <div class="project-icon">
-        <img :src="project.icon">
-        <div class="project-mask">
-          <div class="project-caption">{{ project.caption }}</div>
-        </div>
-      </div>
-      <div class=project-name>
-          {{ project.name }}
-      </div>
+		<div class="project-icon">
+			<img :src="project.icon">
+			<div class="project-mask">
+				<div class="project-caption">
+					{{ project.caption }
+				</div>
+			</div>
+		</div>
+		<div class=project-name>
+			{{ project.name }}
+		</div>
 	</div>
 </div>
 </template>
@@ -19,11 +21,11 @@
 		data(){
 			return {
 				ProductProjects: [
-          {"id": 1, "name": "ホームページ開発プロジェクト", "icon":require("../../../svg/website.svg"), "caption":"coming soon"},
-          {"id": 2, "name": "GAME制作", "icon":require("../../../svg/game.svg"), "caption":"coming soon"},
-          {"id": 3, "name": "アプリ開発", "icon":require("../../../svg/app.svg"), "caption":"coming soon"},
-          {"id": 4, "name": "人工知能", "icon":require("../../../svg/AI.svg"), "caption":"coming soon"}
-        ],
+					{"id": 1, "name": "ホームページ開発プロジェクト", "icon":require("../../../svg/website.svg"), "caption":"coming soon"},
+					{"id": 2, "name": "GAME制作", "icon":require("../../../svg/game.svg"), "caption":"coming soon"},
+					{"id": 3, "name": "アプリ開発", "icon":require("../../../svg/app.svg"), "caption":"coming soon"},
+					{"id": 4, "name": "人工知能", "icon":require("../../../svg/AI.svg"), "caption":"coming soon"}
+				],
 			}
 		},
 		created(){
@@ -34,22 +36,22 @@
 
 <style>
 ul {
-  list-style: none;
+	list-style: none;
 }
 
 .project-icon {
 	width: 60%;
 	overflow:	hidden;
 	position:	relative;	/* 相対位置指定 */
-  margin: auto;
+	margin: auto;
 }
 .project-icon .project-caption {
 	font-size: 6vw;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateY(-50%) translateX(-50%);
-  transform: translateY(-50%) translateX(-50%);
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translateY(-50%) translateX(-50%);
+	transform: translateY(-50%) translateX(-50%);
 	color:			#fff;
 }
 .project-icon .project-mask {
@@ -68,13 +70,13 @@ ul {
 }
 
 .project-name {
-  font-size: 5vw;
-  text-align: center;
+	font-size: 5vw;
+	text-align: center;
 }
 
 .project {
-  margin: 10% 0;
-  white-space: nowrap;
+	margin: 10% 0;
+	white-space: nowrap;
 }
 
 </style>
